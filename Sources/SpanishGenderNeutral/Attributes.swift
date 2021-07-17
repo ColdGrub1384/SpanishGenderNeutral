4e73b3a0b5c916190ffdead5181098cb89a44c0e
Emma Labbé <adrilabbelol@gmail.com>
2021-07-17 15:37:38 -0400

    Fix build error while exporting localization strings

--- Sources/SpanishGenderNeutral/Attributes.swift
+++ Sources/SpanishGenderNeutral/Attributes.swift
@@ -1,4 +1,4 @@
-import SwiftUI
+import Foundation
 
 /// An attribute to specify an alternative to a string when inflecting with an unspecified grammatical gender.
 @available(macOS 12, *) @available(iOS 15, *) @available(tvOS 15, *) @available(watchOS 8, *)
@@ -13,7 +13,6 @@ extension AttributeScopes {
     /// Attributes for `SpanishGenderNeutral`.
     public struct SpanishGenderNeutralAttributes : AttributeScope {
         public let noGrammaticalGender : NoGrammaticalGenderAttribute
-        public let swiftUI: SwiftUIAttributes
     }
     
     /// Attributes for `SpanishGenderNeutral`.
